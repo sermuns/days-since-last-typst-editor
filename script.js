7ccadf081809835c3403786137ee822d0763babf
Samuel Åkesson <sermuns@lysator.liu.se>
2026-08-20 00:49:45 +0200

    fix: clamp negative days since last release to zero

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
 	const now = new Date()
 	const last_release = new Date(days.dataset.lastRelease)
-	const days_since_last_release = Math.floor((now - last_release) / (1000 * 3600 * 24))
+	const days_since_last_release = Math.max(0, Math.floor((now - last_release) / (1000 * 3600 * 24)))
 	days.textContent = days_since_last_release
 	sentenceEnd.textContent = `day${days_since_last_release == 1 ? "" : "s"} since the last release of a Typst editor`
 }
